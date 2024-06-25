a15ea5025f3a2b26abff2c1b7edf858d531a0b11
Ararat Vanyan <ararat_vanyan@bk.ru>
2024-06-26 01:31:18 +0400

    исправлены ошибкиÐ, отделен вывод нзначений из функции sort Ð

--- fs.go
+++ fs.go
@@ -3,36 +3,82 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 	"io/fs"
+	"os"
 	"path/filepath"
 	"sort"
+	"text/tabwriter"
 )
+
+func main() {
+	rootPtr := flag.String("root", "", "Путь до нужной директории")
+	sortPtr := flag.String("sort", "ASK", "Параметр сортировки (возрастание/убывание)")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Использование: go run fs.go --root=<путь_до_нужной_директории> --sort=<параметр_сортировки>\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if *rootPtr == "" || *sortPtr == "" {
+		fmt.Println("Ошибка: пропущены нужные флаги.")
+		flag.Usage()
+		return
+	}
+
+	if !(*sortPtr == "ASK" || *sortPtr == "DESC") {
+		fmt.Println("Ошибка: флаг сорт не может быть с таким значением.")
+		flag.Usage()
+		return
+	}
+
+	dirName := *rootPtr
+	sortMethod := *sortPtr
+
+	propertiesFiles, err := getPropertiesFiles(dirName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	pairs := sortFiles(propertiesFiles, sortMethod)
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 1, 3, ' ', tabwriter.TabIndent)
+
+	fmt.Fprintln(w, "Name\tType\tSize")
+
+	for _, pair := range pairs {
+		fmt.Fprintf(w, "%s\t%s\t%s\n", pair.Key, pair.Value.fileType, formatSize(pair.Value.size))
+	}
+
+	w.Flush()
+}
+
+var nameSizes = [5]string{"b", "Kb", "Mb", "Gb", "Tb"}
 
 // formatSize преобразует размер в байтах к удобному для чтения виду
 func formatSize(size int64) string {
 	s := float64(size)
 
-    nameSizes := []string{"b", "Kb", "Mb", "Gb", "Tb"}
+	if s < 1024 {
+		return fmt.Sprintf("%.2f B", s)
+	}
 
-    if s < 1024 {
-        return fmt.Sprintf("%d B", s)
-    }
+	i := 0
 
-    i := 0
+	for s >= 1024 && i < len(nameSizes)-1 {
+		s /= float64(1024)
+		i++
+	}
 
-    for s >= 1024 && i < len(nameSizes)-1 {
-        s /= float64(1024)
-        i++
-    }
-
-    return fmt.Sprintf("%.2f %s", s, nameSizes[i])
+	return fmt.Sprintf("%.2f %s", s, nameSizes[i])
 }
 
 // determineSize определяет полный размер директории вместе с файлами
 func determineSize(f string) (int64, error) {
 	var size int64
-   
+
 	err := filepath.Walk(f, func(_ string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -46,27 +92,27 @@ func determineSize(f string) (int64, error) {
 	if err != nil {
 		return 0, err
 	}
-	
+
 	return size, nil
 }
 
 // getPropertiesFiles возвращает свойства файлов из заданной директории
-func getPropertiesFiles(dirName string) (map[string]int64, error) {
+func getPropertiesFiles(dirName string) (map[string]fileProperty, error) {
 	files, err := os.ReadDir(dirName)
-    if err != nil {
+	if err != nil {
 		return nil, err
-    }
+	}
 
-	propertiesFiles := map[string]int64{}
+	propertiesFiles := map[string]fileProperty{}
 
-    for _, file := range files {
+	for _, file := range files {
 		fPath := fmt.Sprintf("%s/%s", dirName, file.Name())
 
 		fileInfo, err := os.Stat(fPath)
-        if err != nil {
-            fmt.Println(err)
-            continue
-        }
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 
 		typeFile := "file"
 		size := fileInfo.Size()
@@ -83,80 +129,40 @@ func getPropertiesFiles(dirName string) (map[string]int64, error) {
 			size += s
 		}
 
-		propertiesFiles[fmt.Sprintf("%s\t%s\t", file.Name(), typeFile)] = size
-    }
+		propertiesFiles[file.Name()] = fileProperty{fileType: typeFile, size: size}
+	}
 
 	return propertiesFiles, nil
 }
 
+type fileProperty struct {
+	fileType string
+	size     int64
+}
+
 type Pair struct {
 	Key   string
-	Value int64
+	Value fileProperty
 }
 
-func (p Pair) Less(other Pair) bool {
-	return p.Value < other.Value
-}
-
-func sortFiles(propertiesFiles map[string]int64, sortMethod string) {
+func sortFiles(propertiesFiles map[string]fileProperty, sortMethod string) []Pair {
 	pairs := []Pair{}
 
 	for key, value := range propertiesFiles {
 		pairs = append(pairs, Pair{Key: key, Value: value})
 	}
-	
+
 	if sortMethod == "ASK" {
 		sort.Slice(pairs, func(i, j int) bool {
-			return pairs[i].Value < pairs[j].Value
+			return pairs[i].Value.size < pairs[j].Value.size
 		})
 	}
 
 	if sortMethod == "DESC" {
 		sort.Slice(pairs, func(i, j int) bool {
-			return pairs[i].Value > pairs[j].Value
+			return pairs[i].Value.size > pairs[j].Value.size
 		})
 	}
-	
-	
-	// Выводим отсортированный список ключей и значений
-	for _, pair := range pairs {
-		fmt.Println(pair.Key, formatSize(pair.Value))
-	}
-}
 
-
-func main() {
-	rootPtr := flag.String("root", "", "Путь до нужной директории")
-	sortPtr := flag.String("sort", "ASK", "Параметр сортировки (возрастание/убывание)")
-
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Использование: go run fs.go --root=<путь_до_нужной_директории> --sort=<параметр_сортировки>\n")
-		flag.PrintDefaults()
-	}
-
-	flag.Parse()
-
-	if *rootPtr == "" || *sortPtr == "" {
-		fmt.Println("Error: missing required flags.")
-		flag.Usage()
-		return
-	}
-
-
-	if !(*sortPtr == "ASK" || *sortPtr == "DESC") {
-		fmt.Println("Error: sort can't have that value.")
-		flag.Usage()
-		return
-	}
-
-	dirName := *rootPtr
-	sortMethod := *sortPtr
-
-	propertiesFiles, err := getPropertiesFiles(dirName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	sortFiles(propertiesFiles, sortMethod)
+	return pairs
 }
